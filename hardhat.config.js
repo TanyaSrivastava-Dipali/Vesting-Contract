857971197cbe3b3f2966e46d00e782b2633a9a7f
Tanya Srivastava <100181875+TanyaSrivastava-Dipali@users.noreply.github.com>
2022-03-01 18:52:14 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,17 +3,17 @@ require("@nomiclabs/hardhat-etherscan");
 require("hardhat-prettier");
 
 const Private_Key =
-  "f3207435874fc6fa7df191e3d9adffc435cf29ed28e8f80beb677c5b91bb611f";
+  "Your wallet private key";
 
 module.exports = {
   solidity: "0.8.10",
   networks: {
     rinkeby: {
-      url: `https://rinkeby.infura.io/v3/5a8ba29c44b840b4b5b315cf958266ae`,
+      url: `your infura link`,
       accounts: [`0x${Private_Key}`],
     },
   },
   etherscan: {
-    apiKey: "QDEAN63NZR4KGQSFFG3DHSU5UE2B5SEEVV",
+    apiKey: "your etherscan api key",
   },
 };
